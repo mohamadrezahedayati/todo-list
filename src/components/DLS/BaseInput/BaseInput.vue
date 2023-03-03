dc4a7ca506f5719a6e5c065cdaf5e60fb9fb2659
Mohammadreza Hedayati <M.Hedayati@Alibaba.ir>
2023-03-03 17:16:45 +0330

    chore: scaffold project

---
+++ src/components/DLS/BaseInput/BaseInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <label v-if="label" :for="id" class="font-bold block">
+      {{ label }}
+    </label>
+    <input
+      :id="id"
+      :type="type"
+      :value="modelValue"
+      @input="updateInput"
+      class="border w-full p-1"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+const props = defineProps<{
+  id: {
+    type: String
+    default: ''
+  }
+  label: {
+    type: String
+    default: ''
+  }
+  modelValue: {
+    type: [String, Number]
+    default: ''
+  }
+  type: {
+    type: String
+    default: 'text'
+  }
+}>()
+
+interface Emits {
+  (e: 'update:modelValue', value: number | string): void
+}
+
+const emit = defineEmits<Emits>()
+
+const updateInput = (event) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <BaseInput 
+            label="test"
+            v-model="value"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import BaseInput from "../components/DLS/BaseInput/BaseInput.vue";
+import { ref } from "vue";
+
+const value = ref(null);
+
+</script>
+
+<style scoped>
+
+</style>
